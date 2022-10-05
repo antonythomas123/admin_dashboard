8cf9bbe1c468ebcdcc40fb5d37054cc5dbb07b65
antonyth123 <antonythomas993@gmail.com>
2022-10-05 12:28:04 +0530

    Middle Widgets Added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,9 +4,10 @@ import Sidebar from '../Sidebar/Sidebar'
 import RidesToday from '../Widgets/RidesToday/RidesToday'
 import TopSupplies from '../Widgets/TopSupplies/TopSupplies'
 import SupplyOverview from '../Widgets/SupplyOverview/SupplyOverview'
+import CityInfo from '../Widgets/CityInfo/CityInfo'
+import RevenueBreakdown from '../Widgets/RevenueBreakDown/RevenueBreakdown'
 
 import './Home.css'
-import RevenueBreakdown from '../Widgets/RevenueBreakDown/RevenueBreakdown'
 
 function Home() {
     return (
@@ -25,7 +26,35 @@ function Home() {
               <SupplyOverview />
               <RevenueBreakdown />
             </div>
-            <div className="second"></div>
+            <div className="second">
+              <CityInfo
+                city="Chennai"
+                count="199"
+                completed="33"
+                onTime="3.25"
+                supplies="330"
+                color="#4636f7"
+                depr="-3.7"
+              />
+              <CityInfo
+                city="Mumbai"
+                count="27"
+                completed="2"
+                onTime="4.5"
+                supplies="20"
+                color="#dea233"
+                depr="+2.5"
+              />
+              <CityInfo
+                city="Banglore"
+                count="44"
+                completed="15"
+                onTime="10.1"
+                supplies="150"
+                color="#f720a1"
+                depr="+3.1"
+              />
+            </div>
             <div className="third"></div>
           </div>
         </div>
